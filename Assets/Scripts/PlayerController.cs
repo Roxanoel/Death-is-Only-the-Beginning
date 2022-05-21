57e15aa66c9efcc774d322d82d3a8c28bc7edd3c
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2022-05-21 16:00:36 +0100

    Implemented basic movement
    Basic WASD movement (no rotation yet)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /// <summary>
 /// This class is responsible for managing player movement.
 /// </summary>
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Config params
+    [SerializeField] float speed = 1.0f;
+
+    // Cached references
+    private Rigidbody2D rb;
+    private Vector3 inputDirection;
+    private bool currentInput = false;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
-        
+        CalculateMovementInput();  // Taken from online solution but I might not need to cache the bool, check if it works and then check if I can just do it as return
+        MovePlayer();
+    }
+
+    private void CalculateMovementInput()
+    {
+        if (inputDirection == Vector3.zero)
+        {
+            currentInput = false;
+        }
+        else if (inputDirection != Vector3.zero)
+        {
+            currentInput = true;
+        }
+    }
+
+    private void MovePlayer()
+    {
+        if (currentInput == true)
+        {
+            Vector3 movement = inputDirection.normalized * speed * Time.deltaTime;
+            transform.position += (movement * Time.deltaTime * speed);
+        }
+    }
+    public void OnMove(InputValue value)
+    {
+        inputDirection = value.Get<Vector2>();
     }
 }
