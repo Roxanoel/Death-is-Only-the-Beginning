3e8ff291e1e8a10193bee9b8e0b5de5160958b88
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2022-05-22 21:13:35 +0100

    Fire rate
    Added configurable fire rate to prevent shooting too rapidly

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -7,10 +7,26 @@ public class Shooter : MonoBehaviour
     // Config params
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] Transform gun;
+    [SerializeField] float fireRateInSeconds = 0.25f;
+
+    // Cached refs
+    private float fireRateTimer;
+
+    private void Start()
+    {
+        fireRateTimer = 0;
+    }
 
     public void Shoot()
     {
+        if (fireRateTimer < fireRateInSeconds) return;
         GameObject firedBullet = Instantiate(bulletPrefab, gun.position, Quaternion.identity);   // Rotation will need to change but for now leave as is
         firedBullet.GetComponent<Bullet>().SetBulletDirection(gameObject.transform.up);
+        fireRateTimer = 0;
+    }
+
+    private void Update()
+    {
+        fireRateTimer += Time.deltaTime;
     }
 }
