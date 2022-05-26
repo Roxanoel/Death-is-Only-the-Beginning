235b7000985d2cfc3352326d53ab48727f9cf23c
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2022-05-26 11:42:57 +0100

    Shooting sfx
    Basic shooting sfx

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -10,19 +10,32 @@ public class Shooter : MonoBehaviour
     [SerializeField] float fireRateInSeconds = 0.25f;
     [SerializeField] float damage = 1.0f;
 
+    [Header("SFX and VFX")]
+    [SerializeField] AudioClip[] gunshotSounds;
+
     // Cached refs
     private float fireRateTimer;
+    private AudioSource audioSource;
 
     private void Start()
     {
         fireRateTimer = 0;
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void Shoot()
     {
         if (fireRateTimer < fireRateInSeconds) return;
+
+        // Shoot bullet
         GameObject firedBullet = Instantiate(bulletPrefab, gun.position, Quaternion.identity);   // Rotation will need to change but for now leave as is
         firedBullet.GetComponent<Bullet>().SetBulletDirection(gameObject.transform.up, damage);
+        // Play firing sound
+        if (audioSource != null)
+        {
+            audioSource.PlayOneShot(gunshotSounds[Random.Range(0, gunshotSounds.Length)]);
+        }
+
         fireRateTimer = 0;
     }
 
