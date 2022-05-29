3bfa3b6cb5202e8ff2f940e0129fb26c9aa71f6f
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2022-05-29 08:16:58 +0100

    Added VFX to cinematic
    Added blood VFX to cinematic (causes problems with timeline for now, can be fixed later if the timeline ends up being necessary but I might not even need a timeline)

--- Assets/BossCinematicSound.cs
+++ Assets/BossCinematicSound.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,24 @@ using UnityEngine;
 public class BossCinematicSound : MonoBehaviour
 {
     [SerializeField] AudioClip shotSFX;
+    [SerializeField] Transform target1;
+    [SerializeField] Transform target2;
+    [SerializeField] GameObject bloodVFX;
+
+    int shotCounter = 0;
 
     public void PlayShotSound()
     {
         AudioSource.PlayClipAtPoint(shotSFX, Camera.main.transform.position);
+        shotCounter ++;
+        if (shotCounter == 1)
+        {
+            Instantiate(bloodVFX, target1);
+        }
+        if (shotCounter == 2)
+        {
+            Instantiate(bloodVFX, target2);
+        }
     }
 
 }
