6d8cd19b8101e6ad6516172b25fd5af381639b9d
Roxanoel <74371060+Roxanoel@users.noreply.github.com>
2022-05-30 15:20:22 +0100

    Added SFX for healing

--- Assets/Scripts/HealthPickup.cs
+++ Assets/Scripts/HealthPickup.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class HealthPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip pickupSFX;
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             collision.GetComponent<Health>().HealFully();
+            AudioSource.PlayClipAtPoint(pickupSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }
     }
